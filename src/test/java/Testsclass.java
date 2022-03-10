3a92dd7a5333cdf4d30e7ac212427b85ea2f6a4b
bhargavabhamidipati <bhargavabhamidipati@gmail.com>
2022-03-10 12:59:14 +0100

    Bhargava Bhamidipati test case

--- src/test/java/Testsclass.java
+++ src/test/java/Testsclass.java
@@ -9,38 +9,11 @@ public class Testsclass {
     public static void setup(){
         calculator = new Calculator();
     }
-    // Bhargava Bhamidipati TestCases
+    //Bhargava Bhamidipati TestCase 1
     @Test
-    void testAdd() {
-        assertEquals(28, calculator.add(16, 12));
+    public void testAdd(){
+        assertEquals(4,calculator.add(2,2));
+        assertEquals(0,calculator.add(2,-2));
+        assertEquals(-4,calculator.add(-2,-2));
     }
-    @Test
-    void testAddNeg() {
-        assertEquals(-4, calculator.add(12, -16));
     }
-    @Test
-    void testAddZero() {
-        assertEquals(872, calculator.add(872, 0));
-    }
-    @Test
-    void testAddMax() {
-        assertEquals(Integer.MAX_VALUE - 1, calculator.add(Integer.MAX_VALUE, -1));
-    }
-    @Test
-    void testAddMin() {
-        assertEquals(Integer.MIN_VALUE + 1, calculator.add(Integer.MIN_VALUE, 1));
-    }
-    @Test
-    void testAddMaximums() {
-        assertEquals(-1, calculator.add(Integer.MIN_VALUE, Integer.MAX_VALUE));
-    }
-    @Test
-    void testAddOutOfScopePos() {
-        assertEquals(Integer.MIN_VALUE, calculator.add(Integer.MAX_VALUE, 1)); //wrong result, but expected
-    }
-    @Test
-    void testAddOutOfScopeNeg() {
-        assertEquals(Integer.MAX_VALUE, calculator.add(Integer.MIN_VALUE, -1)); //same
-    }
-
-}
